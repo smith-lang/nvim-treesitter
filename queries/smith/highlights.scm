681f3980b81866221a61169a7404d0c751e3f461
Adam Kowalski <adam.kowalski.work@gmail.com>
2024-07-21 22:18:06 -0700

    add highlights for smith

---
+++ queries/smith/highlights.scm
@@ -0,0 +1,50 @@
+[
+ "|"
+ ">"
+ "<"
+ "+"
+ "-"
+ "*"
+ "/"
+ "?"
+] @operator
+
+[
+ "not"
+] @keyword.operator
+
+[
+ "fn"
+] @keyword.function
+
+[
+ "struct"
+] @keyword.type
+
+[
+ "if"
+ "else"
+] @keyword.conditional
+
+[
+ "for"
+] @keyword.repeat
+
+(integer_literal) @number
+
+(float_literal) @number
+
+(string_literal) @string
+
+(boolean_literal) @boolean
+
+(identifier) @variable
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
